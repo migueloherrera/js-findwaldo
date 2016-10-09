360e204add1dd74a985440a5628d1a766d53ff14
Miguel <miguel.o.herrera@gmail.com>
2016-10-09 15:25:24 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+boards = Board.create([{ name: "easy" }, 
+                       { name: "normal" }, 
+                       { name: "hard" }])
+
+characters = Character.create([
+                      { name: "Waldo", posx: 485, posy: 331, board_id: 1 },
+                      { name: "Odlaw", posx: 221, posy: 335, board_id: 1 },
+                      { name: "Wizard", posx: 583, posy: 335, board_id: 1 },
+                      { name: "Waldo", posx: 454, posy: 243, board_id: 2 },
+                      { name: "Wizard", posx: 0, posy: 225, board_id: 2 },
+                      { name: "Wenda", posx: 664, posy: 272, board_id: 2 },
+                      { name: "Waldo", posx: 678, posy: 228, board_id: 3 },
+                      { name: "Wizard", posx: 317, posy: 246, board_id: 3 },
+                      { name: "Odlaw", posx: 175, posy: 472, board_id: 3 },
+                      { name: "Wenda", posx: 272, posy: 462, board_id: 3 } 
+                        ])
